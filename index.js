6e6741905a43aaeb3a131aac0c6a732b8323cf66
amirn003 <amir.naar@gmail.com>
2023-11-14 09:28:38 +0100

    calculate full time in hours, mins and secs

--- index.js
+++ index.js
@@ -7,4 +7,12 @@ const seconds = timeNodes
     return mins * 60 + secs;
     // return parseInt(timeCode.split(':')[0]) * 60 + parseInt(timeCode.split(':')[1]) ;
   })
-  ;
+  .reduce((total, vidSeconds) => total + vidSeconds);
+
+let secondLeft = seconds;
+const hours = Math.floor(secondLeft / 3600);
+secondLeft = secondLeft % 3600;
+const mins = Math.floor(secondLeft / 60);``
+secondLeft = secondLeft % 60;
+
+console.log("Time ===> ", hours, mins, secondLeft);
